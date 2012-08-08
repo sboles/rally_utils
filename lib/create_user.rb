f5d344c25324ec8a9db96d46c04d7a8210e5ff53
Steven Boles <boles.steven@gmail.com>
2012-08-08 13:14:04 -0400

    removed comments

--- lib/create_user.rb
+++ lib/create_user.rb
@@ -29,17 +29,3 @@ def switch_toggles(cookie, subscription_id, toggles)
     post('http://localhost:7001/slm/admin/tools/setSubscriptionToggle.sp', cookie, form_string)
   end
 end
-
-# ADMIN_USER = ARGV[0] || 'test@test.com'
-# ALM_MODULES = ARGV[1].split('|') || []
-# TOGGLES = ARGV[2].split('|') || []
-# 
-# puts ADMIN_USER
-# puts ALM_MODULES
-# puts TOGGLES
-# 
-# cookie = login('slmadmin', 'w0rk$h0p')
-# 
-# subscription_id = create_subscription(cookie, ADMIN_USER, ALM_MODULES)
-# 
-# switch_toggles(cookie, subscription_id, TOGGLES)
