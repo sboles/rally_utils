da654605cf2cf4c34e7adb0ee9b5fa067a11d69f
stefankendall <stefankendall@gmail.com>
2012-09-28 16:36:25 -0400

    Made branch name inline editable

--- extensions/better_rally/js/kanban/add_implemented_in_field_to_cards.js
+++ extensions/better_rally/js/kanban/add_implemented_in_field_to_cards.js
@@ -1,35 +1,90 @@
 (function () {
+    var enableInlineEditForBranchName = function () {
+        if (!$(this).find('input').is(":visible")) {
+            var $readOnlyBranchName = $(this).find('.readOnlyBranchName');
+            var branchName = $readOnlyBranchName.text();
+
+            var $inlineEditInput = $(this).find('input');
+            $inlineEditInput.val(branchName);
+            $inlineEditInput.prop('disabled', false);
+
+            $readOnlyBranchName.hide();
+            $inlineEditInput.show();
+        }
+    };
+
+    var inlineEditInputKeyEvent = function (e) {
+        var ENTER_KEY_CODE = 13;
+        if ((e.keyCode || e.which) == ENTER_KEY_CODE) {
+            var $modifiedInput = $(this);
+            $modifiedInput.prop('disabled', true);
+
+            var newBranchName = $modifiedInput.val();
+            var formattedId = getFormattedIdForCard($($modifiedInput.parents('.card')[0]));
+            queryForArtifact(formattedId, function (record) {
+                var fieldToChange = record.store.model.elementName == "HierarchicalRequirement" ? 'ImplementedIn' : 'FixedInBuild';
+                record.set(fieldToChange, newBranchName);
+                record.save({callback:function () {
+                    $modifiedInput.hide();
+                    var $readOnlyBranchName = $($modifiedInput.parents('.branchIndicator')[0]).find('.readOnlyBranchName');
+                    $readOnlyBranchName.text(newBranchName);
+                    $readOnlyBranchName.show();
+                }});
+            });
+        }
+    };
+
+    var getFormattedIdForCard = function ($card) {
+        return $card.find('.leftCardHeader').text();
+    };
+
+    var getModelNameFromFormattedId = function (formattedId) {
+        return formattedId.substring(0, 1) == "S" ? "HierarchicalRequirement" : "Defect";
+    };
+
+    var queryForArtifact = function (formattedId, callback) {
+        var modelName = getModelNameFromFormattedId(formattedId);
+        Rally.data.ModelFactory.getModel({
+            type:modelName,
+            success:function (model) {
+                model.find({
+                    filters:[
+                        {
+                            property:'FormattedID',
+                            value:formattedId
+                        }
+                    ],
+                    callback:callback
+                });
+            }
+        });
+    };
+
     var addImplementedInFieldToCards = function (d) {
         var COLUMNS_TO_ADD_FIELD_TO = ['Building', 'Testing', 'Merging'];
         $(COLUMNS_TO_ADD_FIELD_TO).each(function (i, header) {
             var $cards = $('.columnHeader:contains("' + header + '")', d).parents('.column').find('.card');
             $cards.each(function () {
                 var $card = $(this);
-                if ($card.find('.branchName').length === 0) {
-                    $card.find('.cardName').after("<hr/><div class='branchName'></div>");
+                if ($card.find('.branchIndicator').length === 0) {
+                    $card.find('.cardName').after("<hr/><div class='branchIndicator'></div>");
 
-                    var cardFormattedId = $(this).find('.leftCardHeader').text();
-                    var modelName = cardFormattedId.substring(0, 1) == "S" ? "HierarchicalRequirement" : "Defect";
-                    Rally.data.ModelFactory.getModel({
-                        type:modelName,
-                        success:function (model) {
-                            model.find({
-                                filters:[
-                                    {
-                                        property:'FormattedID',
-                                        value:cardFormattedId
-                                    }
-                                ],
-                                callback:function (record) {
-                                    var branchName = modelName == "HierarchicalRequirement" ? record.get('ImplementedIn') : record.get("FixedInBuild");
-                                    var branchHtml = "<strong>Branch:</strong> " + branchName;
-                                    $card.find('.branchName').html(branchHtml);
-                                    $card.find('.branchName').bind('selectstart', function (e) {
-                                        e.stopPropagation();
-                                    });
-                                }
-                            });
-                        }
+                    var cardFormattedId = getFormattedIdForCard($(this));
+                    queryForArtifact(cardFormattedId, function (record) {
+                        var modelName = record.store.model.elementName;
+                        var branchName = modelName == "HierarchicalRequirement" ? record.get('ImplementedIn') : record.get("FixedInBuild");
+                        var branchHtml = "<strong>Branch:</strong> " +
+                            "<span class='branchName'>" +
+                            "<span class='readOnlyBranchName'>" + branchName + "</span> " +
+                            "<input class='editBranchName' style='display:none' type='text'/>" +
+                            "</span>";
+                        $card.find('.branchIndicator').html(branchHtml);
+
+                        $card.find('.branchIndicator').dblclick(enableInlineEditForBranchName);
+                        $card.find('.editBranchName').keypress(inlineEditInputKeyEvent);
+                        $card.find('.branchIndicator').bind('selectstart', function (e) {
+                            e.stopPropagation();
+                        });
                     });
                 }
             });
