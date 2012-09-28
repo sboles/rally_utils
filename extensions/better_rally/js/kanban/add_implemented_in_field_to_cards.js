fe3f680da4782369911ec269da836b951cbc2cd5
stefankendall <stefankendall@gmail.com>
2012-09-28 16:00:41 -0400

    Added branch indicator to building and testing columns.

--- extensions/better_rally/js/kanban/add_implemented_in_field_to_cards.js
+++ extensions/better_rally/js/kanban/add_implemented_in_field_to_cards.js
@@ -1,35 +1,38 @@
 (function () {
     var addImplementedInFieldToCards = function (d) {
-        var $cards = $('.columnHeader:contains("Merging")', d).parents('.column').find('.card');
-        $cards.each(function () {
-            var $card = $(this);
-            if ($card.find('.branchName').length === 0) {
-                $card.find('.cardName').after("<hr/><div class='branchName'></div>");
+        var COLUMNS_TO_ADD_FIELD_TO = ['Building', 'Testing', 'Merging'];
+        $(COLUMNS_TO_ADD_FIELD_TO).each(function (i, header) {
+            var $cards = $('.columnHeader:contains("' + header + '")', d).parents('.column').find('.card');
+            $cards.each(function () {
+                var $card = $(this);
+                if ($card.find('.branchName').length === 0) {
+                    $card.find('.cardName').after("<hr/><div class='branchName'></div>");
 
-                var cardFormattedId = $(this).find('.leftCardHeader').text();
-                var modelName = cardFormattedId.substring(0, 1) == "S" ? "HierarchicalRequirement" : "Defect";
-                Rally.data.ModelFactory.getModel({
-                    type:modelName,
-                    success:function (model) {
-                        model.find({
-                            filters:[
-                                {
-                                    property:'FormattedID',
-                                    value:cardFormattedId
+                    var cardFormattedId = $(this).find('.leftCardHeader').text();
+                    var modelName = cardFormattedId.substring(0, 1) == "S" ? "HierarchicalRequirement" : "Defect";
+                    Rally.data.ModelFactory.getModel({
+                        type:modelName,
+                        success:function (model) {
+                            model.find({
+                                filters:[
+                                    {
+                                        property:'FormattedID',
+                                        value:cardFormattedId
+                                    }
+                                ],
+                                callback:function (record) {
+                                    var branchName = modelName == "HierarchicalRequirement" ? record.get('ImplementedIn') : record.get("FixedInBuild");
+                                    var branchHtml = "<strong>Branch:</strong> " + branchName;
+                                    $card.find('.branchName').html(branchHtml);
+                                    $card.find('.branchName').bind('selectstart', function (e) {
+                                        e.stopPropagation();
+                                    });
                                 }
-                            ],
-                            callback:function (record) {
-                                var branchName = modelName == "HierarchicalRequirement" ? record.get('ImplementedIn') : record.get("FixedInBuild");
-                                var branchHtml = "<strong>Branch:</strong> " + branchName;
-                                $card.find('.branchName').html(branchHtml);
-                                $card.find('.branchName').bind('selectstart', function (e) {
-                                    e.stopPropagation();
-                                });
-                            }
-                        });
-                    }
-                });
-            }
+                            });
+                        }
+                    });
+                }
+            });
         });
     };
 
