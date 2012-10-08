12750558ee44686c80acb552236bdd146dab2289
stefankendall <stefankendall@gmail.com>
2012-10-08 14:53:07 -0400

    Fixed script to properly push short branch name.

--- bin/prune_old_branches.rb
+++ bin/prune_old_branches.rb
@@ -8,9 +8,10 @@ branch_prune_count = 0
 branches.each do |branch|
   branch_date = Date.parse `git log -1 #{$branch} --format=%ci`
 
-  puts "#{branch_date} #{branch}"
+  branch_name = branch.split('remotes/crazytrain/')[1]
+  puts "#{branch_date} #{branch_name}"
   if branch_date < prune_date
-    system "git push crazytrain :${branch}"
+    system "git push crazytrain :${branch_name}"
     branch_prune_count = branch_prune_count + 1
   end
 end
