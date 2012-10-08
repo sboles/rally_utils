2117ba0e7e57f8614fa23c6adec7d5157c8f956c
stefankendall <stefankendall@gmail.com>
2012-10-08 15:03:34 -0400

    Fixed script to properly delete branches.

--- bin/prune_old_branches.rb
+++ bin/prune_old_branches.rb
@@ -1,17 +1,17 @@
 require 'date'
 
-PRUNE_DAYS = 14
+PRUNE_DAYS = 30
 prune_date = Date.today - PRUNE_DAYS
 
 branches = `git branch -a | grep crazytrain/`.split /\n/
 branch_prune_count = 0
 branches.each do |branch|
-  branch_date = Date.parse `git log -1 #{$branch} --format=%ci`
+  branch_date = Date.parse `git log -1 #{branch} --format=%ci`
 
   branch_name = branch.split('remotes/crazytrain/')[1]
   puts "#{branch_date} #{branch_name}"
   if branch_date < prune_date
-    system "git push crazytrain :${branch_name}"
+    system "git push crazytrain :#{branch_name}"
     branch_prune_count = branch_prune_count + 1
   end
 end
