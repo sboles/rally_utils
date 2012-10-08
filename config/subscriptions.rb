c3639bc9c0a6eea56ca6fc9041e05c05fde0577d
stefankendall <stefankendall@gmail.com>
2012-10-08 16:59:39 -0400

    added subscription_type to create_subscription

---
+++ config/subscriptions.rb
@@ -0,0 +1,21 @@
+# Default subscriptions that will be created in dev
+
+module RallyUtils
+  SUBSCRIPTIONS = {}
+  SUBSCRIPTIONS[:admin_username] = 'slmadmin'
+  SUBSCRIPTIONS[:admin_password] = 'w0rk$h0p'
+  SUBSCRIPTIONS[:subscriptions] = [
+      {
+          :username => 'admin@ee.com',
+          :modules => ['Rally Portfolio Manager', 'Rally Quality Manager'],
+          :subscription_type => ['Enterprise'],
+          :toggles => ['ENABLE_BULK_EDIT_OF_ALL_FIELDS', 'PROVISION_TRIAL_AND_COMMUNITY_IN_PROD']
+      },
+      {
+          :username => 'admin@xe.com',
+          :modules => [],
+          :subscription_type => ['Express_Edition'],
+          :toggles => []
+      }
+  ]
+end
--- features/support/env.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH << File.expand_path('../../../lib', __FILE__)
-
-require 'rally_utils.rb'
-
-Before do
-end
-
-After do
-end
