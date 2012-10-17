44cdeefd9469a4037e454695a6e3d5a2399f5646
Steven Boles <boles.steven@gmail.com>
2012-10-17 17:48:37 -0400

    added polling for iframes

--- extensions/better_rally/js/kanban/remove_unnecessary_filters.js
+++ extensions/better_rally/js/kanban/remove_unnecessary_filters.js
@@ -1,31 +1,13 @@
 (function () {
+
     var removeFilters = function (d) {
-        var $appHeader = $('#appHeader', d);
-        var $userStorySelector = $('#userStories', d);
-        var $defectSelector = $('#defects', d);
-        var $filterSelector = $('.filterByTagsDropdown', d);
-
-        var readyToRemove = $appHeader.length !== 0 && $userStorySelector.length !== 0 && $defectSelector !== 0 && $filterSelector !== 0;
-        if (!readyToRemove) {
-            return false;
-        }
-
-        $appHeader.remove();
-        $userStorySelector.remove();
-        $defectSelector.remove();
-        $filterSelector.remove();
-
-        return readyToRemove;
+        $('#appHeader', d).remove();
+        $('#userStories', d).remove();
+        $('#defects', d).remove();
+        $('.filterByTagsDropdown', d).remove();
+        return true;
     };
 
-    setInterval(function (event) {
-        if ($('iframe').length == 0 || $('iframe').data('filters-removed')) {
-            return;
-        }
+    RallyUtil.waitForIframeElementsAndExecute(['#appHeader', '#userStories', '#defects', '.filterByTagsDropdown' ], removeFilters);
 
-        $('iframe').each(function () {
-            var filtersRemoved = removeFilters($(this)[0].contentDocument);
-            $(this).data('filters-removed', filtersRemoved);
-        });
-    }, 2000);
 })();
