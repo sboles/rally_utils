38f8fdd9384468a3a759b55ed2021f0ea9392623
Steven Boles <boles.steven@gmail.com>
2012-10-18 13:44:48 -0400

    fixed iframe for realz

--- extensions/better_rally/js/util/polling.js
+++ extensions/better_rally/js/util/polling.js
@@ -1,23 +1,23 @@
 (function () {
     var DEFAULT_POLL_INTERVAL = 2000;
 
-    var waitForElementsAndExecute = function (selectors, command, interval) {
-        interval = interval || DEFAULT_POLL_INTERVAL;
+    var waitForElementsAndExecute = function (selectors, command, d) {
+        d = d || document;
         var pollingId = setInterval(function () {
             var shouldExecute = true;
             _.each(selectors, function (selector) {
-                if ($(selector).length === 0) {
+                if ($(selector, d).length === 0) {
                     shouldExecute = false;
                 }
             });
 
             if (shouldExecute) {
-                var completed = command();
+                var completed = command(d);
                 if (completed) {
                     clearInterval(pollingId);
                 }
             }
-        }, interval);
+        }, DEFAULT_POLL_INTERVAL);
     };
 
     var pollForever = function (command, interval) {
@@ -28,9 +28,7 @@
     var waitForIframeElementsAndExecute = function (selectors, command) {
         waitForElementsAndExecute(['iframe.rally-html'], function () {
                 var iframe_document = $('iframe.rally-html')[0].contentDocument;
-                waitForElementsAndExecute(selectors, function () {
-                    return command(iframe_document);
-                });
+                waitForElementsAndExecute(selectors, command, iframe_document);
                 return true;
             }
         );
