604c0be46f651ce5effb8c74c82f40ba6fe123f4
Matt McClellan <4d415454@gmail.com>
2012-11-12 10:10:12 -0500

    re-added 'no entry'

--- extensions/better_rally/js/editors/filter_owners.js
+++ extensions/better_rally/js/editors/filter_owners.js
@@ -5,12 +5,10 @@
             return;
         }
 
-        var noEntry = $(w.document).find('select[name = "owner"] option[value = "NULL_CRITERIA"]');
         var ownerGroups = $(w.document).find('optgroup');
         var nonCrazyGroup = ownerGroups[1];
 
         $(nonCrazyGroup).remove();
-        $(noEntry).remove();
     };
 
     RallyUtil.pollForever(function () {
