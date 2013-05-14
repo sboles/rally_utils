2d28ab4ce70b74f101b5970fe0d260ecf76031d4
Steven Boles <boles.steven@gmail.com>
2013-05-14 09:26:37 -0400

    added infrastructure from rules

---
+++ infrastructure/install_intellij_12.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+./common.sh
+wget http://download.jetbrains.com/idea/ideaIU-12.1.2.dmg
+hdiutil mount ideaIU-12.1.2.dmg
+cp -R /Volumes/IntelliJ\ IDEA\ 12/IntelliJ\ IDEA\ 12.app /Applications
+hdiutil unmount /Volumes/IntelliJ\ IDEA\ 12
+#open /Applications/IntelliJ\ IDEA\ 12.app
+mv ~/Library/Preferences/IntelliJIdea12/idea.vmoptions ~/Library/Preferences/IntelliJIdea12/idea.vmoptions.$(date '+%Y-%m-%d_%H-%M-%S')
+echo '-Xms512m
+-Xmx4000m
+-XX:MaxPermSize=500m
+-XX:ReservedCodeCacheSize=96m
+-XX:+UseCodeCacheFlushing
+-XX:+UseCompressedOops' >> ~/Library/Preferences/IntelliJIdea12/idea.vmoptions
+rm ideaIU-12.1.2.dmg
