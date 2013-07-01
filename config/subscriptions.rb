905d64d5d556a4882eae428c1c5c773faa127f49
Stefan Kendall <stefankendall@gmail.com>
2013-07-01 14:23:48 -0400

    Removed unneeded subs, added detail page opt in to created sub

--- config/subscriptions.rb
+++ config/subscriptions.rb
@@ -6,28 +6,10 @@ module RallyUtils
   SUBSCRIPTIONS[:admin_password] = 'w0rk$h0p'
   SUBSCRIPTIONS[:subscriptions] = [
       {
-          :username => 'admin@hs.com',
-          :modules => [],
-          :subscription_type => 'HS_1',
-          :toggles => []
-      },
-      {
-          :username => 'admin@ce.com',
-          :modules => [],
-          :subscription_type => 'HS_1',
-          :toggles => ['F2577_ADD_STORY_DEFECT_ASSOCIATION_TO_COMMUNITY_EDITION', 'F2578_TWO_LEVEL_HIERARCHY']
-      },
-      {
           :username => 'admin@ee.com',
           :modules => ['Rally Portfolio Manager', 'Rally Quality Manager'],
           :subscription_type => 'Enterprise',
-          :toggles => ['ENABLE_BULK_EDIT_OF_ALL_FIELDS', 'PROVISION_TRIAL_AND_COMMUNITY_IN_PROD','IMPORT_TASKS','IMPORT_TEST_CASES_WITH_RELATIONSHIPS']
-      },
-      {
-          :username => 'admin@xe.com',
-          :modules => [],
-          :subscription_type => 'Express_Edition',
-          :toggles => []
+          :toggles => ['ENABLE_BULK_EDIT_OF_ALL_FIELDS', 'PROVISION_TRIAL_AND_COMMUNITY_IN_PROD','IMPORT_TASKS','IMPORT_TEST_CASES_WITH_RELATIONSHIPS', 'JS_DETAIL_PAGE']
       }
   ]
 end
