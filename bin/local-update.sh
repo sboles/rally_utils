5c751d576c45dc1f076a62634fd0cca9ca6f6955
Steven Boles and Ben Wittenberg <sboles@rallydev.com>
2014-02-07 15:23:59 -0500

    Added script to update local environment.

---
+++ bin/local-update.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo_and_run() {
+    tput setaf 4;
+    echo "[local-update]: $@";
+    tput sgr0;
+    "$@";
+}
+
+git_update() {
+    echo_and_run git add -A .
+    echo_and_run git stash;
+    echo_and_run git checkout master;
+    echo_and_run git pull origin master;
+}
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+
+fi
+
+
+
+# update oh-my-zsh-custom
+echo_and_run cd ~/.oh-my-zsh-custom
+git_update
+
+
+# update appsdk
+echo_and_run cd ~/projects/appsdk
+git_update
+echo_and_run npm install
+echo_and_run grunt build
+
+
+# update alm
+APPSDK_PATH=~/projects/appsdk
+echo_and_run cd ~/projects/alm/alm-webapp
+git_update
+echo_and_run npm install
+echo_and_run grunt build
+echo_and_run buildr clean jetty:run
