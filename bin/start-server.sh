1cd41c5af657c0d8820079136e6699e406814fef
Ben Wittenberg <bwittenberg@rallydev.com>
2014-04-22 10:35:23 -0400

    newBranch updated to only branch, script added to start server.

---
+++ bin/start-server.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env zsh
+
+echo_and_run() {
+    tput setaf 4
+    echo "[start-server]: $@"
+    tput sgr0
+    "$@"
+}
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+
+fi
+
+
+# update oh-my-zsh-custom
+echo_and_run cd ~/.oh-my-zsh-custom
+echo_and_run rvm rvmrc load
+
+
+# update appsdk
+echo_and_run cd ~/projects/appsdk
+echo_and_run rvm rvmrc load
+echo_and_run npm install
+echo_and_run grunt build
+
+
+# update app-catalog
+echo_and_run cd ~/projects/app-catalog
+echo_and_run rvm rvmrc load
+echo_and_run npm install
+echo_and_run grunt build
+
+
+# update alm
+echo_and_run export APPSDK_PATH=${HOME}/projects/appsdk
+echo_and_run export APPCATALOG_PATH=${HOME}/projects/app-catalog
+echo_and_run cd ~/projects/alm/alm-webapp
+echo_and_run rvm rvmrc load
+echo_and_run npm install
+echo_and_run grunt build
+echo_and_run buildr db:migrations
+echo_and_run buildr clean jetty:run
