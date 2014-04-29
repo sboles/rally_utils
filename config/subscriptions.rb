40351345f9dee95653da0f177947cb7e5504fc44
Steven Boles <sboles@rallydev.com>
2014-04-29 09:00:21 -0400

    added UE subscription and toggle to enable edp by default

--- config/subscriptions.rb
+++ config/subscriptions.rb
@@ -1,6 +1,7 @@
 # Default subscriptions that will be created in dev
 
 module RallyUtils
+  TOGGLES = ['ENABLE_BULK_EDIT_OF_ALL_FIELDS', 'PROVISION_TRIAL_AND_COMMUNITY_IN_PROD','IMPORT_TASKS','IMPORT_TEST_CASES_WITH_RELATIONSHIPS', 'NEW_FEEDBACK_STYLING', 'HARMONIZE_ALM_UI', 'CRAZYTRAIN_F4090_EDP_LAYOUT', 'F4514_COMPLETE_EDP_LAYOUT', 'CRAZYTRAIN_F4089_EDP_BETA', 'EDP_TASK_BETA', 'EDP_DEFECT_BETA','EDP_BETA_SET_PREFERENCE_ON_BY_DEFAULT']
   SUBSCRIPTIONS = {}
   SUBSCRIPTIONS[:admin_username] = 'slmadmin'
   SUBSCRIPTIONS[:admin_password] = 'w0rk$h0p'
@@ -9,7 +10,13 @@ module RallyUtils
           :username => 'admin@ee.com',
           :modules => ['Rally Portfolio Manager', 'Rally Quality Manager'],
           :subscription_type => 'Enterprise',
-          :toggles => ['ENABLE_BULK_EDIT_OF_ALL_FIELDS', 'PROVISION_TRIAL_AND_COMMUNITY_IN_PROD','IMPORT_TASKS','IMPORT_TEST_CASES_WITH_RELATIONSHIPS', 'NEW_FEEDBACK_STYLING', 'HARMONIZE_ALM_UI', 'CRAZYTRAIN_F4090_EDP_LAYOUT', 'F4514_COMPLETE_EDP_LAYOUT', 'CRAZYTRAIN_F4089_EDP_BETA', 'EDP_TASK_BETA', 'EDP_DEFECT_BETA']
+          :toggles => TOGGLES 
+      },
+      {
+          :username => 'admin@ue.com',
+          :modules => ['Rally Portfolio Manager', 'Rally Quality Manager'],
+          :subscription_type => 'Unlimited',
+          :toggles => TOGGLES 
       }
   ]
 end
